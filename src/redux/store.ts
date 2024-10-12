9116726519d0a7e3898e8e6f2172bb4a1bfaa5f2
Jakub Jelínek <jakub.f.jelinek@gmail.com>
2024-10-13 00:11:00 +0200

    Feature(system): Redux Toolkit store - from .js to .ts

--- src/redux/store.ts
+++ src/redux/store.ts
@@ -17,3 +17,5 @@ export const store = configureStore({
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware().concat(api.middleware),
 });
+
+export type RootState = ReturnType<typeof store.getState>;
